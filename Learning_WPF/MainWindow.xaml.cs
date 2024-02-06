e5e7535001bd44416dec7423c1206d842196b8f4
ChadM95 <ChadM95@users.noreply.github.com>
2024-02-06 18:36:10 +0000

    add click event handler and logic to button

---
+++ Learning_WPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Learning_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool running = false;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //stop
+                tbToggle.Text = "Stopped";
+                btn.Content = "Run";
+            }
+
+            else
+            {
+                //run
+                tbToggle.Text = "Running...";
+                btn.Content = "Stop";
+            }
+#
+            //flips boolean
+            running = !running;
+        }
+    }
+}
