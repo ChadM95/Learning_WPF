a670a26114715103861da11c1fb8b921c1bd70ff
ChadM95 <ChadM95@users.noreply.github.com>
2024-03-01 19:34:56 +0000

    clear project

--- Learning_WPF/MainWindow.xaml.cs
+++ Learning_WPF/MainWindow.xaml.cs
@@ -16,41 +16,7 @@ namespace Learning_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        //random object
-        Random random = new Random();
-        int randomNum1, randomNum2;
-
-        //start game button click event
-        private void btnStart_Click(object sender, RoutedEventArgs e)
-        {
-            randomNum1 = random.Next(0,21);
-            answer1.Text = Convert.ToString(randomNum1);
-
-        }
-
-
-        //second number button click event
-        private void btnGuess_Click(object sender, RoutedEventArgs e)
-        {
-            randomNum2 = random.Next(0, 21);
-            answer2.Text = Convert.ToString(randomNum2);
-
-
-            //check radio buttons against answer
-
-            bool biggerSelected = rbBigger.IsChecked.Equals(true);
-            bool smallerSelected = rbSmaller.IsChecked.Equals(true);
-
-            if (biggerSelected && randomNum1 < randomNum2)
-                MessageBox.Show("YOU WIN!!");
-            else if (biggerSelected && randomNum1 > randomNum2)
-                MessageBox.Show("YOU LOSE!!");
-            else if (smallerSelected && randomNum1 > randomNum2)
-                MessageBox.Show("YOU WIN!!");
-            else if (smallerSelected && randomNum1 < randomNum2)
-                MessageBox.Show("YOU LOSE!!");
-
-        }
+        
 
     }
 }
