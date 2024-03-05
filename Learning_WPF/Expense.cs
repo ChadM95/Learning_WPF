b62ee4acd2a26d47118c1fc9bdd9a2b30e8dbf8b
ChadM95 <ChadM95@users.noreply.github.com>
2024-03-05 21:36:37 +0000

    labsheet 3 exercise 1

---
+++ Learning_WPF/Expense.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_WPF
+{
+    class Expense
+    {
+        public string Category { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime DateIncurred { get; set; }
+
+        public Expense(string category, decimal cost, DateTime dateIncurred)
+        {
+            Category = category;
+            Cost = cost;
+            DateIncurred = dateIncurred;
+        }
+
+        public Expense()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Category} {Cost:C} {DateIncurred}";
+        }
+    }
+}
