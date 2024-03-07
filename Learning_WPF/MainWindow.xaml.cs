03b645b6e6af71548219a21ac9df5cfee78c7d3a
ChadM95 <ChadM95@users.noreply.github.com>
2024-03-07 21:08:32 +0000

    Labsheet 4 / Exercise 1 / part 3 - Json read/write files

--- Learning_WPF/MainWindow.xaml.cs
+++ Learning_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Windows;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace Learning_WPF
 {
@@ -129,6 +131,39 @@ namespace Learning_WPF
 
                 //update display
                 lbxExpenses.ItemsSource = matchingExpenses;
+            }
+
+        }
+
+        //save expense objects / JSON
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //get string of objects - JSON formatted
+            string json = JsonConvert.SerializeObject(expenses,Formatting.Indented);
+
+            //write to file
+            using (StreamWriter sw = new StreamWriter(@"c:\temp\expenses.json"))
+            {
+                sw.Write(json);
+            }
+
+        }
+
+        //loads json file
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            //connect to a file
+            using (StreamReader sr = new StreamReader(@"c:\temp\expenses.json"))
+            {
+                //read text
+                string json = sr.ReadToEnd();
+
+                //convert from JSON to objects
+                expenses = JsonConvert.DeserializeObject<ObservableCollection<Expense>>(json);
+
+                //refresh display
+                lbxExpenses.ItemsSource = expenses;
+
             }
 
         }
